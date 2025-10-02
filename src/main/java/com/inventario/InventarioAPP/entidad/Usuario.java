3b4f3cdd9ce486d7d3dc13940f9867076aa98d0a
yurany <bityura25@gmail.com>
2025-10-01 22:48:50 -0500

    se actualiza validacion del login

--- src/main/java/com/inventario/InventarioAPP/entidad/Usuario.java
+++ src/main/java/com/inventario/InventarioAPP/entidad/Usuario.java
@@ -1,15 +1,31 @@
 package com.inventario.InventarioAPP.entidad;
 
-import lombok.Data;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
 @Data
 @Entity
-@Table(name = "usuarios")
+@Table(name = "usuarios", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "nombreUsuario") // evita duplicados
+})
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    @Size(min = 3, max = 50, message = "El nombre de usuario debe tener entre 3 y 50 caracteres")
+    @Column(nullable = false, unique = true, length = 50)
     private String nombreUsuario;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 4, message = "La contraseña debe tener mínimo 4 caracteres")
+    @Column(nullable = false)
     private String password;
-    private String rol; // ADMIN o USER
+
+    @NotBlank(message = "El rol es obligatorio")
+    @Column(nullable = false, length = 20)
+    private String rol; // Ejemplo: ADMIN o USER
 }
